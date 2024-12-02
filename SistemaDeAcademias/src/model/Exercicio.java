92edb95b3ac5916da38b15feed049c1803b0bf4a
Hugo Santos <hugosantos.ufg@gmail.com>
2024-12-02 12:04:28 -0300

    feat= added class exercicio

--- SistemaDeAcademias/src/model/Exercicio.java
+++ SistemaDeAcademias/src/model/Exercicio.java
@@ -1,5 +1,44 @@
 package model;
 
 public class Exercicio {
+    private String nome;
+    private int series;
+    private int repeticoes;
+    
+    public Exercicio(String nome, int series, int repeticoes) {
+        this.nome = nome;
+        this.series = series;
+        this.repeticoes = repeticoes;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    public int getRepeticoes() {
+        return repeticoes;
+    }
+
+    public void setRepeticoes(int repeticoes) {
+        this.repeticoes = repeticoes;
+    }
+
+    @Override
+    public String toString() {
+        return "Exercicio [nome=" + nome + ", series=" + series + ", repeticoes=" + repeticoes + "]";
+    }
+
 
 }
